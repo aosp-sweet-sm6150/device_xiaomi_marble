138138aaa16fc2326b5208dbdfcec08fdf2ab614
lemezohaib <zhbkhan644@gmail.com>
2024-04-24 09:31:33 +0000

    marble: Welcome AwakenOS!
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- awaken_marble.mk
+++ awaken_marble.mk
@@ -9,9 +9,27 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 
 # Inherit from common lineage configuration
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_marble
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Google Recorder
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Google Assistant
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+#Pixel Charger
+USE_PIXEL_CHARGER := true
+
+#official
+AWAKEN_BUILD_TYPE := official
+
+#face unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_NAME := awaken_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO

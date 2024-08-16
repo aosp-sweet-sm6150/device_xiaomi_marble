5b2538bf03044c46795362808a909c3e17a12d56
lemezohaib <zhbkhan644@gmail.com>
2024-08-16 16:56:48 +0200

    marble: Adapt for PixelOS!
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- aosp_marble.mk
+++ aosp_marble.mk
@@ -7,10 +7,13 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common aosp configuration
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_marble
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_marble
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO

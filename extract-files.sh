ca8ac6fdd45b094a0aaa4d49deb0bb9017303fd9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-11-26 03:02:37 +0530

    marble: Import Dolby Atmos blobs
    also use patchelf 0.18

--- extract-files.sh
+++ extract-files.sh
@@ -17,7 +17,15 @@ function blob_fixup() {
             ;;
         vendor/lib64/libcamximageformatutils.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
+            "${PATCHELF_0_18}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
+            ;;
+        vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_18}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            ;;
+        vendor/lib64/hw/audio.primary.taro.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF_0_18}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
         *)
             return 1
